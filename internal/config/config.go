74c656b4d957d40b0d17a399baf8733e43ee851a
NooraPanahi <nourap064@gmail.com>
2026-09-04 19:32:33 +0330

    feat: add application configuration

---
+++ internal/config/config.go
@@ -0,0 +1,24 @@
+package config
+
+import "os"
+
+type Config struct {
+	Port        string
+	DatabaseURL string
+}
+
+func Load() Config {
+	return Config{
+		Port: getEnv("PORT", "8080"),
+		DatabaseURL: getEnv("DATABASE_URL", "user=web password=web dbname=web host=localhost port=5433 sslmode=disable"),
+	}
+} 
+
+func getEnv(key, defaultvalue string) string {
+	value := os.Getenv(key)
+
+	if value == "" {
+		return defaultvalue
+	}
+	return  value
+}
