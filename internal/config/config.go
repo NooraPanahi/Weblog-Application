05f10e0905f33370dc46fb7af5c06ba59e14b499
NooraPanahi <nourap064@gmail.com>
2026-09-06 19:25:05 +0330

    feat: add session configuration

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,14 +3,16 @@ package config
 import "os"
 
 type Config struct {
-	Port        string
-	DatabaseURL string
+	Port          string
+	DatabaseURL   string
+	SessionSecret string
 }
 
 func Load() Config {
 	return Config{
 		Port: getEnv("PORT", "8080"),
 		DatabaseURL: getEnv("DATABASE_URL", "user=web password=web dbname=web host=localhost port=5433 sslmode=disable"),
+		SessionSecret: getEnv("SESSION_SECRET", "development-secret-blah-blah"),
 	}
 } 
 
