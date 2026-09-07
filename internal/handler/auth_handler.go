d957018c4f8d9648d8e3bd84ab288b5726d98d7f
NooraPanahi <nourap064@gmail.com>
2026-09-07 19:09:27 +0330

    feat: add login functions

--- internal/handler/auth_handler.go
+++ internal/handler/auth_handler.go
@@ -55,3 +55,32 @@ func (h *AuthHandler) Register(c echo.Context) error {
 
 	return c.Redirect(http.StatusSeeOther, "/")
 }
+
+func (h *AuthHandler) ShowLogin (c echo.Context) error {
+	return c.Render(http.StatusOK, "login.html", nil)
+}
+
+func (h *AuthHandler) Login (c echo.Context) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	user, err := h.authService.Login(username, password)
+	if err != nil {
+		if errors.Is(err, service.ErrInvalidCredentials){ 
+			return c.Render(http.StatusUnauthorized,
+				"login.html", map[string]string{
+					"Error": "Invalid username or password",
+				},
+			)			
+		}
+
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to login")
+		
+	}
+
+	if err := h.sessionManager.SetUserID(c.Response().Writer, c.Request(), user.ID); err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to create session")
+	}
+
+	return c.Redirect(http.StatusSeeOther, "/")	
+}
