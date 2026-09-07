bafd7dd13e25795e46aecd35b3d086843578831e
NooraPanahi <nourap064@gmail.com>
2026-09-07 13:48:16 +0330

    feat: add authentication handler

---
+++ internal/handler/auth_handler.go
@@ -0,0 +1,57 @@
+package handler
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/NooraPanahi/Weblog-Application.git/internal/service"
+	"github.com/NooraPanahi/Weblog-Application.git/internal/session"
+	"github.com/labstack/echo/v4"
+)
+
+type AuthHandler struct {
+	authService    *service.AuthService
+	sessionManager *session.Manager
+}
+
+func NewAuthHandler(authService *service.AuthService, sessionManager *session.Manager) *AuthHandler {
+	return &AuthHandler{
+		authService:    authService,
+		sessionManager: sessionManager}
+}
+
+func (h *AuthHandler) ShowRegister(c echo.Context) error {
+	return c.Render(http.StatusOK, "register.html", nil)
+}
+
+func (h *AuthHandler) Register(c echo.Context) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	user, err := h.authService.Register(username, password)
+	if err != nil {
+		switch {
+		case errors.Is(err, service.ErrUsernameTaken):
+			return c.Render(http.StatusConflict,
+				"register.html", map[string]interface{}{
+					"Error": "Username is already taken",
+				},
+			)
+		case errors.Is(err, service.ErrInvalidInput):
+			return c.Render(http.StatusBadRequest,
+				"register.html", map[string]interface{}{
+					"Error": "invalid username or password",
+				},
+			)
+
+		default:
+			return echo.NewHTTPError(http.StatusInternalServerError, "failed to register user")
+		}
+	}
+
+	if err := h.sessionManager.SetUserID(c.Response().Writer, c.Request(), user.ID); err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to create session")
+	}
+
+	return c.Redirect(http.StatusSeeOther, "/")
+}
