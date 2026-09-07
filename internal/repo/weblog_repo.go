5aed458714147a6b9af69dd50c91d988a74b1d74
NooraPanahi <nourap064@gmail.com>
2026-09-07 22:50:41 +0330

    feat: add listVisible function to weblog repo

--- internal/repo/weblog_repo.go
+++ internal/repo/weblog_repo.go
@@ -11,7 +11,6 @@ type WeblogRepo struct {
 	db *sql.DB
 }
 
-
 func NewWeblogRepo(db *sql.DB) *WeblogRepo {
 	return &WeblogRepo{db: db}
 }
@@ -21,21 +20,21 @@ func (we *WeblogRepo) Create(weblog *model.Weblog) error {
 			  VALUES ($1, $2, $3, $4, $5)
 			  RETURNING id, created_at`
 
-	err := we.db.QueryRow(query,weblog.Title, weblog.Content, weblog.AuthorID, weblog.Privacy).Scan(&weblog.ID, &weblog.CreatedAt)
+	err := we.db.QueryRow(query, weblog.Title, weblog.Content,weblog.Image, weblog.AuthorID, weblog.Privacy).Scan(&weblog.ID, &weblog.CreatedAt)
 
 	if err != nil {
-		return fmt.Errorf("create user: %w", err)
+		return fmt.Errorf("create weblog: %w", err)
 	}
 	return nil
 }
 
-func (r *WeblogRepo) FindWeblogByUserID(id int64) (*model.Weblog, error) {
+func (r *WeblogRepo) FindWeblogByID(id int64) (*model.Weblog, error) {
 	query := `SELECT id, title, content, image, author_id, privacy, created_at
 			  FROM weblogs WHERE id = $1`
 
 	weblog := &model.Weblog{}
 
-	err := r.db.QueryRow(query, id).Scan( &weblog.ID, &weblog.Title, &weblog.Content, &weblog.Image,
+	err := r.db.QueryRow(query, id).Scan(&weblog.ID, &weblog.Title, &weblog.Content, &weblog.Image,
 		&weblog.AuthorID, &weblog.Privacy, &weblog.CreatedAt)
 
 	if err != nil {
@@ -43,4 +42,38 @@ func (r *WeblogRepo) FindWeblogByUserID(id int64) (*model.Weblog, error) {
 	}
 
 	return weblog, nil
+}
+
+func (r *WeblogRepo) ListVisible(userID int64) ([]*model.Weblog, error) {
+	query := `SELECT w.id, w.title, w.content, w.image, w.author_id, w.privacy, w.created_at
+			  FROM weblogs w 
+			  LEFT JOIN weblog_shares ws
+			  ON ws.weblog_id = w.id AND ws.user_id = $1
+			  WHERE w.privacy = 'public' OR w.author_id = $1
+			  OR ws.user_id IS NOT NULL
+			  ORDER BY w.created_at DESC`
+
+	rows, err := r.db.Query(query, userID)
+	if err != nil {
+		return  nil, err
+	}
+
+	defer rows.Close()
+
+	var weblogs []*model.Weblog
+
+	for rows.Next() {
+		weblog := &model.Weblog{}
+		err := rows.Scan(&weblog.ID, &weblog.Title, &weblog.Content, &weblog.Image, &weblog.AuthorID, &weblog.Privacy, &weblog.CreatedAt)
+
+		if err != nil {
+			return nil, err
+		}
+		weblogs = append(weblogs, weblog)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return weblogs, nil
 }
