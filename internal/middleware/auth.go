6b4b6f49a2c26cae32dba821159726ba8da870d0
NooraPanahi <nourap064@gmail.com>
2026-09-08 12:07:36 +0330

    update auth middleware to use sessions

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,11 +1,14 @@
 package middleware
 
 import (
+	"net/http"
+
+	"github.com/NooraPanahi/Weblog-Application.git/internal/repo"
 	"github.com/NooraPanahi/Weblog-Application.git/internal/session"
 	"github.com/labstack/echo/v4"
 )
 
-func RequireAuth(sessionManager *session.Manager) echo.MiddlewareFunc {
+func RequireAuth(sessionManager *session.Manager, userRepo *repo.UserRepo) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			userID, ok, err := sessionManager.GetUserID(c.Request())
@@ -17,7 +20,14 @@ func RequireAuth(sessionManager *session.Manager) echo.MiddlewareFunc {
 			if !ok {
 				return c.Redirect(302, "/login")
 			}
-			c.Set("user_id", userID)
+
+			user, err := userRepo.FindUserByID(userID)
+
+			if err != nil {
+				return c.Redirect(http.StatusSeeOther, "/login")
+			}
+			c.Set("userID", user.ID)
+			c.Set("username", user.Username)
 			return next(c)
 
 		}
